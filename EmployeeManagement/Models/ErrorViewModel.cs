b652b089075097d4da686fa90c592878a1097620
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-07-27 17:52:27 +0900

    変更

--- EmployeeManagement/Models/ErrorViewModel.cs
+++ EmployeeManagement/Models/ErrorViewModel.cs
@@ -7,5 +7,12 @@ namespace EmployeeManagement.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Title { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Cd { get; set; }
+        public bool Publish { get; set; }
     }
 }
