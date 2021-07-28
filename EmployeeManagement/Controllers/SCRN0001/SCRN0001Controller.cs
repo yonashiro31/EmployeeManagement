0f48ecccc9ecf6c8bb79065d49e0e7d351d248db
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-07-28 18:04:45 +0900

    変更

---
+++ EmployeeManagement/Controllers/SCRN0001/SCRN0001Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers.SCRN0001
+{
+    public class SCRN0001Controller : Controller
+    {
+        [Route("")]
+        [Route("/employee/menu")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/SCRN0002/SCRN0002Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers.SCRN0002
+{
+    public class SCRN0002Controller : Controller
+    {
+        [Route("entry")]
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
