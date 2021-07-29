0ba210909bd710c71c65c9789804e7798dad95ba
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-07-29 18:08:33 +0900

    変更

--- EmployeeManagement/Controllers/SCRN0001/SCRN0001Controller.cs
+++ EmployeeManagement/Controllers/SCRN0001/SCRN0001Controller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagement.Helper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,26 @@ namespace EmployeeManagement.Controllers.SCRN0001
 {
     public class SCRN0001Controller : Controller
     {
-        [Route("")]
-        [Route("/employee/menu")]
-        [HttpGet]
-        public IActionResult Index()
+
+        //社員更新画面（SCRN0003）表示時、エラーが設定されていた場合は受け取る
+       
+
+        /// <summary>
+        ///画面制御
+        /// </summary>
+    ///<remarks>
+    ///画面制御を行う
+    /// </remarks>
+    [Route("")]
+    [Route("/employee/menu")]
+    [HttpGet]
+    public IActionResult Index()
         {
+            
+            EV0001Helper instanceEV0001Helper = new EV0001Helper();
+            instanceEV0001Helper.Init();
             return View();
         }
+
     }
 }
