93d9333563bc423fcf6efc8ff9614f6dc961e067
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-08-16 17:48:26 +0900

    変更

--- EmployeeManagement/ViewModel/SCRN0002ViewModel.cs
+++ EmployeeManagement/ViewModel/SCRN0002ViewModel.cs
@@ -7,7 +7,6 @@ namespace EmployeeManagement.ViewModel
 {
     public class SCRN0002ViewModel
     {
-
         public SCRN0002ViewModel()
         {
             ErrorMessageList = new List<ErrorMessageModel>();
@@ -15,7 +14,12 @@ namespace EmployeeManagement.ViewModel
         }
         public string EmployeeID { set; get; }
         public IList<ErrorMessageModel> ErrorMessageList { get; set; }
-        public string Busyo { get; set; }
-        public string Gender { get; set;}
+        public string AffiliationCd { get; set; }
+        public string PositionCd { get; set; }
+        public string EmployeeName { get; set; }
+        public string Gender { get; set; }
+        public string BirthDay { get; set; }
+        public bool ForeignNationality { get; set; }
+        public string BaseSalary { get; set; }
     }
 }
